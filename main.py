771e5ae2ae40a76c7d22df8c89117505db55932e
crazeoni <40152649+crazeoni@users.noreply.github.com>
2024-11-29 02:41:36 +0100

    tasksXXXVI

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ CORS(app)
 
 
 # Path to the tasks JSON file
-TASKS_FILE = os.path.join(os.path.dirname(__file__), "tasks.json")
+TASKS_FILE = "tasks.json"
 USER_SCORES_FILE = os.path.join(os.path.dirname(__file__), "user_scores.json")
 
 TELEGRAM_BOT_TOKEN = "7425794811:AAEmTeMbQa94UmWnTOyiNAn-rS7hdZO_1OA"
@@ -19,29 +19,29 @@ TELEGRAM_BOT_TOKEN = "7425794811:AAEmTeMbQa94UmWnTOyiNAn-rS7hdZO_1OA"
 
 
 
-@app.route("/api/send-data", methods=["POST"])
-def send_data():
-    data = request.json  # Get data from Web App
-    print(f"Received data: {data}")
-    CHAT_ID = data.get("chat_id", None)
+# @app.route("/api/send-data", methods=["POST"])
+# def send_data():
+    # data = request.json  # Get data from Web App
+    # print(f"Received data: {data}")
+    # CHAT_ID = data.get("chat_id", None)
 
-    if "username" in data and "message" in data:
-        # Construct message to send to Telegram bot
-        bot_message = f"Received data from {data['username']}: {data['message']}"
+    # if "username" in data and "message" in data:
+        # # Construct message to send to Telegram bot
+        # bot_message = f"Received data from {data['username']}: {data['message']}"
 
-        # Send message to bot
-        if CHAT_ID:  # Ensure CHAT_ID is set
-            send_message_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-            payload = {"chat_id": CHAT_ID, "text": bot_message}
-            response = requests.post(send_message_url, json=payload)
-            if response.ok:
-                return jsonify({"message": "Data processed and sent to bot!"}), 200
-            else:
-                return jsonify({"error": "Failed to send message to bot!"}), 500
-        else:
-            return jsonify({"error": "Chat ID is not set!"}), 400
-    else:
-        return jsonify({"error": "Invalid data"}), 400
+        # # Send message to bot
+        # if CHAT_ID:  # Ensure CHAT_ID is set
+            # send_message_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+            # payload = {"chat_id": CHAT_ID, "text": bot_message}
+            # response = requests.post(send_message_url, json=payload)
+            # if response.ok:
+                # return jsonify({"message": "Data processed and sent to bot!"}), 200
+            # else:
+                # return jsonify({"error": "Failed to send message to bot!"}), 500
+        # else:
+            # return jsonify({"error": "Chat ID is not set!"}), 400
+    # else:
+        # return jsonify({"error": "Invalid data"}), 400
 
 
 # Helper functions
@@ -132,5 +132,13 @@ def get_balance():
     total_rewards = sum(task["reward"] for task in tasks if task["completed"])
     return jsonify({"total": total_rewards}), 200
 
+# with open("tasks.json", "w") as file:
+    # json.dump([{"id": 11, "completed": True}], file, indent=4)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+
+
+
+
