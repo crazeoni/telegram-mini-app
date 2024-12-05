65779d2ce265301aa4d92c2cfb8b59e6b6b1fd37
crazeoni <40152649+crazeoni@users.noreply.github.com>
2024-12-06 00:21:07 +0100

    connectionIVYI

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:ozioma235@localhost:5433/scream_db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://scream_user:ozioma235@localhost:5433/scream_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
