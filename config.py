60690c7a37fbbe66a063857e7cc8e6967e2610ab
crazeoni <40152649+crazeoni@users.noreply.github.com>
2024-12-06 01:58:43 +0100

    connectionIVYIXXXX

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://scream_user:ozioma235@https://telegram-mini-app-7nu1.onrender.com:5433/scream_db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://scream_user:ozioma235@telegram-mini-app-7nu1.onrender.com:5433/scream_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
