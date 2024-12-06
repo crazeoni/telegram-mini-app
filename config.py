660f31197d8862c7508f085c4162717e92be2ad5
crazeoni <40152649+crazeoni@users.noreply.github.com>
2024-12-06 07:14:35 +0100

    render dbII

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://scream_user:4zWTA445paDT9zyCoe8GJlhXeQOtSHnn@dpg-ct988t9opnds73e59aa0-a/scream_db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://scream_tg_user:N0R00VqPu6eOW907DMqJqtqzfP4uxhiR@dpg-ct99c056l47c73aovre0-a/scream_database'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
