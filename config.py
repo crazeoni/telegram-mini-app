0720d198e7d3cfcc587dd4cf0710d43b83015307
crazeoni <40152649+crazeoni@users.noreply.github.com>
2024-12-06 08:52:05 +0100

    unknown host fixed

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://mide:zXG47ShjSMytebiUBMD1igRHmXcSCiKz@dpg-ct99mtt6l47c73ap5hd0-a/mide_db_qact'
+SQLALCHEMY_DATABASE_URI = 'postgresql://mide:zXG47ShjSMytebiUBMD1igRHmXcSCiKz@dpg-ct99mtt6l47c73ap5hd0-a.oregon-postgres.render.com/mide_db_qact'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
